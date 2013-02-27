fe46f8b869b852e75e30101327dfd284ccdb0ad6
Shigenobu Nishikawa <snishikawa@aiming-inc.com>
2013-02-27 20:30:58 +0900

    implement insert

--- lib/vendor_machine.rb
+++ lib/vendor_machine.rb
@@ -1,7 +1,10 @@
 class VendorMachine
+  attr_accessor :sum
   def initialize
+    @sum = 0
   end
 
   def insert(money)
+    @sum += money
   end
 end
--- spec/vendor_machine_spec.rb
+++ spec/vendor_machine_spec.rb
@@ -1,8 +1,21 @@
 require_relative '../lib/vendor_machine'
 describe VendorMachine do
-  describe '#insert' do
-    it 'succeeds' do
-      expect { subject.insert(100) }.not_to raise_error
+  describe '投入できること' do
+    context '100円入れた時' do
+      before do
+        subject.insert(100)
+      end
+      it '合計が100円になる' do
+        subject.sum.should == 100
+      end
+      context 'さらに1,000円入れた時' do
+        before do
+          subject.insert(1000)
+        end
+        it '合計が1100円になる' do
+          subject.sum.should == 1100
+        end
+      end
     end
   end
 end
